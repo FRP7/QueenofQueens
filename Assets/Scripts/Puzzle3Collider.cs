e13ddba454a533ac171c7ae3047af713663db42e
FRP7 <frelvaspires@gmail.com>
2018-12-15 19:39:45 +0000

    puzzle das rodinhas (wip)
    ja estou a programar mas ainda nao funciona

---
+++ Assets/Scripts/Puzzle3Collider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle3Collider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "SphereCollider")
+		{
+			Debug.Log("Colidiu!");
+		}
+	}
+	
+}
